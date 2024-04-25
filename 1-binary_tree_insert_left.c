fb4b21e28708e223b746f57f31b70e928a10b45f
DentistOmarAshraf <omar_ashraf@live.com>
2024-04-25 08:18:29 +0000

    1-binary_tree_insert_left.c

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - insert only left to the parent
+ * @parent: binary_tree_t node (if NULL return NULL)
+ * @value: int (to be inserted in binary_tree_t node->n)
+ * Return: binary_tree_t pointer
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (!parent)
+		return (NULL);
+
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+	new->n = value;
+	new->parent = parent;
+
+	if (parent->left)
+	{
+		new->left = parent->left;
+		parent->left->parent = new;
+		parent->left = new;
+		return (new);
+	}
+	parent->left = new;
+	new->left = NULL;
+	new->right = NULL;
+	return (new);
+}
