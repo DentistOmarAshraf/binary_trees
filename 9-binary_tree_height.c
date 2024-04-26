efb028a493bb461f0ab1c28b77a667cc820035fa
DentistOmarAshraf <omar_ashraf@live.com>
2024-04-26 18:09:34 +0000

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,12 +8,13 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left, right;
+	size_t left, right;
 
-	if (!tree)
-		return (-1);
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
-	return (1 + (left > right ? left : right));
+	return ((left > right ? left : right) + 1);
 }
