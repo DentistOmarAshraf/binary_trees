4454eaad63f285f1c975c03e6d458f7e2b8b7abf
DentistOmarAshraf <omar_ashraf@live.com>
2024-04-26 23:35:30 +0000

    betty

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,4 +1,9 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_leaves - count how many leaves
+ * @tree: binary_tree_t Node
+ * Return: size_t
+ */
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
