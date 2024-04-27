b6788519e5d292f76b8346e3e2e8eb8f1c67f590
DentistOmarAshraf <omar_ashraf@live.com>
2024-04-27 16:45:57 +0000

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,22 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_height - calculate height of binary tree
+ * this function is not as same as in task (9)
+ * cause its not stop when it meat a leaf
+ * @tree: binary_tree_t node
+ * Return: size_t (height)
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (!tree)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	return ((left > right ? left : right) + 1);
+}
+/**
  * binary_tree_balance - check if the node has tow child
  * @tree: binary_tree_t node
  * Return: int
@@ -7,9 +24,10 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (!tree || (!tree->left && !tree->right))
-		return (0);
-	if (tree->left && tree->right)
-		return (2);
-	return (-1);
+	size_t left, right;
+
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+
+	return (left - right);
 }
